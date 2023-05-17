568d166e7a1b11b9e18181bb8c34e48ecb2fc876
kaktuz <kaktuz911@gmail.com>
2023-05-17 17:07:07 +0300

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,10 +1,12 @@
 export function fetchCountries(name) {
-    const country = fetch(`https://restcountries.com/v3.1/name/${name}`);
-return country.then((response) => {
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name.official,population,flags.svg,languages`)
+    .then((response) => {
     return response.json();
   })
   .then((data) => {
+    
     console.log(data);
+    return data;
   })
   .catch ((error) => {
 console.error('Ошибка:', error);
